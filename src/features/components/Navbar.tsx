e55b75d7b2031e94b638b09a4947ff897534f468
BALTASARGD <https://github.com/BALTASARGD>
2025-03-20 13:28:42 +0100

    Actualiza el título de la página y mejora el estilo de la barra de navegación con nuevos estilos y enlaces activos

--- src/features/components/Navbar.tsx
+++ src/features/components/Navbar.tsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../../Styles/navbar.css"; 
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/skills">Skills</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
+        <li><NavLink to="/skills" className={({ isActive }) => isActive ? "active" : ""}>Skills</NavLink></li>
+        <li><NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}>Projects</NavLink></li>
+        <li><NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>Contact</NavLink></li>
       </ul>
     </nav>
   );
