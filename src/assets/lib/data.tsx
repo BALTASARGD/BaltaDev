97f23ea33d1a9a31a9dd3ebe9df431a1bd3066ef
BALTASARGD <https://github.com/BALTASARGD>
2025-04-02 13:09:32 +0200

    Remove unused CSS files and update email links in sidebar data.

--- src/assets/lib/data.tsx
+++ src/assets/lib/data.tsx
@@ -1,5 +1,5 @@
 import { FiLinkedin, FiGithub, FiMail } from 'react-icons/fi';
-import adobexdicon from '../icons/adobexdicon.svg';
+
 import htmlicon from '../icons/htmlicon.svg';
 import cssicon from '../icons/cssicon.svg';
 import javascripticon from '../icons/javascripticon.svg';
@@ -8,64 +8,29 @@ import sassscssicon from '../icons/sassicon.svg';
 import reacticon from '../icons/reacticon.svg';
 import nodejsicon from '../icons/nodejsicon.svg';
 import tailwindcssicon from '../icons/tailwindicon.svg';
-import figmaicon from '../icons/figmaicon.svg';
-import canvaicon from '../icons/canvaicon.svg';
 
 export const sideBarRightMail = {
-    link: "mailto:myemail@com",
-    text: "baltakoeln@icloud.com",
-  } as const;
+  link: "mailto:baltakoeln@icloud.com",
+  text: "baltakoeln@icloud.com",
+} as const;
 
 export const sideBarLeftSocials = [
-    {
-      link: "https://www.linkedin.com/in/Baltademonio/",
-      icon: FiLinkedin,
-      altimgname: "linkedin",
-    },
-    {
-      link: "https://github.com/BALTASARGD",
-      icon: FiGithub,
-      altimgname: "github",
-    },
-    {
-      link: "mailto:myemail@com",
-      icon: FiMail,
-      altimgname: "mail",
-    },
-  ] as const;
-
-export const headerIntroData = {
-  title: {
-    de: "Hi, ich bin Balt",
-    en: "Hi, I'm Balta",
+  {
+    link: "https://www.linkedin.com/in/Baltademonio/",
+    icon: FiLinkedin,
+    altimgname: "linkedin",
   },
-  subtitle: "Fullstack Developer ",
-  description: {
-    de: "Ich bin Balta, ein Fullstack-Entwickler mit dem Ziel, meine Karriere voranzutreiben und an inspirierenden Projekten teilzunehmen. Hier präsentiere ich meine Arbeiten und meine Leidenschaft für die Webentwicklung. Lassen Sie uns gemeinsam digitale Lösungen gestalten und die Zukunft formen!",
-    en: "I'm Balta, a fullstack developer with the goal of advancing my career and participating in inspiring projects. Here I present my work and my passion for web development. Let's shape digital solutions together and shape the future!",
+  {
+    link: "https://github.com/BALTASARGD",
+    icon: FiGithub,
+    altimgname: "github",
   },
-  buttons: [
-    {
-      name: "Contact",
-      label: {
-        de: "Kontaktiere mich",
-        en: "Contact me",
-      },
-      icon: FiMail,
-      color: "main-btn",
-    },
-    {
-      name: "Projects",
-      label: {
-        de: "Meine Projekte",
-        en: "My Projects",
-      },
-      icon: FiGithub,
-      color: "secondary-btn",
-    },
-  ],
-  profilepicture: "./src/assets/images/FotoPerfil.jpeg",
-} as const;
+  {
+    link: "mailto:baltakoeln@icloud.com",
+    icon: FiMail,
+    altimgname: "mail",
+  },
+] as const;
 
 export const skillsDataWeb = [
   {
